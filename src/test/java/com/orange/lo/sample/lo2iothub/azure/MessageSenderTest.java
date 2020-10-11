e10d693efe5f092677d3641d97d402a4090cfd82
Szymon Lisiecki <szymon.lisiecki@likims.com>
2020-10-11 21:53:25 +0200

    static mock

--- src/test/java/com/orange/lo/sample/lo2iothub/azure/MessageSenderTest.java
+++ src/test/java/com/orange/lo/sample/lo2iothub/azure/MessageSenderTest.java
@@ -1,9 +1,15 @@
 package com.orange.lo.sample.lo2iothub.azure;
 
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import com.microsoft.azure.sdk.iot.device.DeviceClient;
-import com.microsoft.azure.sdk.iot.device.IotHubClientProtocol;
 import com.orange.lo.sample.lo2iothub.utils.Counters;
-import io.micrometer.core.instrument.Counter;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,13 +18,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.support.GenericMessage;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import io.micrometer.core.instrument.Counter;
 
 @ExtendWith(MockitoExtension.class)
 class MessageSenderTest {
@@ -29,21 +29,21 @@ class MessageSenderTest {
     private Counters counterProvider;
     @Mock
     private Counter counter;
+    @Mock
     private DeviceClient deviceClient;
     private MessageSender messageSender;
 
     @BeforeEach
     void setUp() throws URISyntaxException {
         messageSender = new MessageSender(counterProvider);
-        deviceClient = new DeviceClient(CONNECTION_STRING, IotHubClientProtocol.MQTT);
     }
 
     @Test
     void sendMessage() throws IOException {
-//        when(counterProvider.evtAttempt()).thenReturn(counter);
-//        Message<String> message = new GenericMessage<>("{\"metadata\":{\"source\":\"iot-device-id\"}}");
-//
-//        messageSender.sendMessage(message, deviceClient);
-//        verify(counterProvider, times(1)).evtAttempt();
+        when(counterProvider.evtAttempt()).thenReturn(counter);
+        Message<String> message = new GenericMessage<>("{\"metadata\":{\"source\":\"iot-device-id\"}}");
+
+        messageSender.sendMessage(message, deviceClient);
+        verify(counterProvider, times(1)).evtAttempt();
     }
 }
