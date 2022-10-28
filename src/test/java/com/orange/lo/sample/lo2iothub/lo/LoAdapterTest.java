43424af9fa4f28caaf3df2e427261629de5848df
grzegorzswistak <grzegorz.swistak2@gmail.com>
2022-10-28 09:03:24 +0200

    Synchronization of devices from all subgroups of a given group

--- src/test/java/com/orange/lo/sample/lo2iothub/lo/LoAdapterTest.java
+++ src/test/java/com/orange/lo/sample/lo2iothub/lo/LoAdapterTest.java
@@ -14,18 +14,30 @@ import com.orange.lo.sdk.rest.devicemanagement.GetGroupsFilter;
 import com.orange.lo.sdk.rest.devicemanagement.Groups;
 import com.orange.lo.sdk.rest.devicemanagement.Inventory;
 import com.orange.lo.sdk.rest.model.Device;
+import com.orange.lo.sdk.rest.model.Group;
 import net.jodah.failsafe.RetryPolicy;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -33,11 +45,20 @@ import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class LoAdapterTest {
-
-    private static final String GROUP_NAME = "root";
     private static final List<Device> DEVICES = Arrays.asList(
             new Device().withId("device-one").withName("device one"),
-            new Device().withId("device-two").withName("device two")
+            new Device().withId("device-two").withName("device two"),
+            new Device().withId("device-three").withName("device three"),
+            new Device().withId("device-four").withName("device four"),
+            new Device().withId("device-five").withName("device five")
+    );
+    private static final List<Group> GROUP_INSTANCES_FROM_LO = Arrays.asList(
+            new Group().withId("root").withParentId(null).withPathNode(null),
+            new Group().withId("aaTBvZUu").withParentId("root").withPathNode("device_group1"),
+            new Group().withId("bbTBvZUu").withParentId("root").withPathNode("device_group2"),
+            new Group().withId("ccTBvZUu").withParentId("bbTBvZUu").withPathNode("device_group2_sub"),
+            new Group().withId("ddTBvZUu").withParentId("bbTBvZUu").withPathNode("device_group2_sub2"),
+            new Group().withId("eeTBvZUu").withParentId("ddTBvZUu").withPathNode("device_group2_sub2_sub")
     );
     private static final int PAGE_SIZE = 20;
 
@@ -56,18 +77,93 @@ class LoAdapterTest {
         when(loApiClient.getDeviceManagement()).thenReturn(deviceManagement);
         when(deviceManagement.getInventory()).thenReturn(inventory);
         when(deviceManagement.getGroups()).thenReturn(groups);
-        when(groups.getGroups(any(GetGroupsFilter.class))).thenReturn(new ArrayList<>());
-        when(inventory.getDevices(any(GetDevicesFilter.class))).thenReturn(DEVICES);
+        when(groups.getGroups(any(GetGroupsFilter.class))).thenReturn(GROUP_INSTANCES_FROM_LO);
+        when(inventory.getDevices(any(GetDevicesFilter.class))).thenAnswer(LoAdapterTest::getObjects);
 
         this.loAdapter = new LoAdapter(loApiClient, PAGE_SIZE, new RetryPolicy<>(), new RetryPolicy<>());
     }
 
-    @Test
-    void shouldCorrectlyCallRestTemplateWhenGettingDevicesData() {
-        List<Device> devices = loAdapter.getDevices(GROUP_NAME);
-
-        assertEquals(2, devices.size());
-        verify(groups, times(1)).getGroups(any(GetGroupsFilter.class));
-        verify(inventory, times(1)).getDevices(any(GetDevicesFilter.class));
+    @NotNull
+    private static List<?> getObjects(InvocationOnMock invocation) {
+        GetDevicesFilter argument = (GetDevicesFilter) invocation.getArguments()[0];
+        switch (argument.getGroupId()) {
+            case "aaTBvZUu":
+                return Collections.singletonList(DEVICES.get(0));
+            case "bbTBvZUu":
+                return Collections.singletonList(DEVICES.get(1));
+            case "ccTBvZUu":
+                return Collections.singletonList(DEVICES.get(2));
+            case "eeTBvZUu":
+                return Collections.singletonList(DEVICES.get(3));
+            case "root":
+                return Collections.singletonList(DEVICES.get(4));
+            default:
+                return new ArrayList<>();
+        }
     }
+
+    @Test
+    void shouldGetOneDeviceWhenGroupWithOneDeviceAndNoSubgroupsIsUsed() {
+        List<Device> devices = loAdapter.getDevices("device_group2_sub2_sub");
+
+        assertEquals(1, devices.size());
+        Device device = devices.get(0);
+        assertEquals("device-four", device.getId());
+
+    }
+
+    @Test
+    void shouldGetWholeDevicesFromSelectedGroupAndSubgroupsWhenGroupWithSubgroupsIsUsed() {
+        List<Device> devices = loAdapter.getDevices("device_group2");
+
+        assertEquals(3, devices.size());
+
+        List<String> expectedDeviceIds = Arrays.asList("device-two", "device-three", "device-four");
+        List<String> actualDeviceIds = devices.stream().map(Device::getId).collect(Collectors.toList());
+        assertTrue(listEqualsIgnoreOrder(expectedDeviceIds, actualDeviceIds));
+    }
+
+    @Test
+    void shouldGetWholeDevicesFromWholeGroupsWhenRootGroupIsUsed() {
+        List<Device> devices = loAdapter.getDevices("root");
+
+        assertEquals(5, devices.size());
+
+        List<String> expectedDeviceIds = DEVICES.stream().map(Device::getId).collect(Collectors.toList());
+        List<String> actualDeviceIds = devices.stream().map(Device::getId).collect(Collectors.toList());
+        assertTrue(listEqualsIgnoreOrder(expectedDeviceIds, actualDeviceIds));
+    }
+
+    @Test
+    void shouldGetWholeDevicesFromWholeGroupsWhenNonExistentGroupIsUsed2() {
+        List<Device> devices = loAdapter.getDevices("NonExistentGroup");
+
+        assertEquals(5, devices.size());
+
+        List<String> expectedDeviceIds = DEVICES.stream().map(Device::getId).collect(Collectors.toList());
+        List<String> actualDeviceIds = devices.stream().map(Device::getId).collect(Collectors.toList());
+        assertTrue(listEqualsIgnoreOrder(expectedDeviceIds, actualDeviceIds));
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideTestData")
+    void shouldAddMonitoringPartForProdEnvironment(String group, int numberOfGroupApiCalls, int numberOfInventoryApiCalls) {
+        List<Device> devices = loAdapter.getDevices(group);
+
+        verify(groups, times(numberOfGroupApiCalls)).getGroups(any(GetGroupsFilter.class));
+        verify(inventory, times(numberOfInventoryApiCalls)).getDevices(any(GetDevicesFilter.class));
+    }
+
+    private static Stream<Arguments> provideTestData() {
+        return Stream.of(
+                Arguments.of("root", 1, 6),
+                Arguments.of("device_group2_sub2_sub", 1, 1),
+                Arguments.of("device_group2", 1, 4)
+        );
+    }
+
+    public static <T> boolean listEqualsIgnoreOrder(List<T> list1, List<T> list2) {
+        return new HashSet<>(list1).equals(new HashSet<>(list2));
+    }
+
 }
