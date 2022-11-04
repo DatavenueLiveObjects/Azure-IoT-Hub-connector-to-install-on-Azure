c0e1297d70540b87d4396cd467e33adea0fe4071
Grzegorz Świstak <61158617+grzegorzswistak@users.noreply.github.com>
2022-11-04 06:36:40 +0100

    Synchronization of devices from all subgroups of a given group (#14)
    * Synchronization of devices from all subgroups of a given group
    
    * Changing the device synchronization interval to 24h in IoT Hub

--- src/main/java/com/orange/lo/sample/lo2iothub/lo/LoAdapter.java
+++ src/main/java/com/orange/lo/sample/lo2iothub/lo/LoAdapter.java
@@ -26,6 +26,9 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class LoAdapter {
 
@@ -34,7 +37,7 @@ public class LoAdapter {
     private static final String DEFAULT_GROUP_ID = "root";
 
     private final LOApiClient loApiClient;
-    private final Map<String, String> groupsMap;
+    private final Map<String, Group> groupsMap;
     private final int pageSize;
     private final RetryPolicy<List<Group>> groupRetryPolicy;
     private final RetryPolicy<List<Device>> deviceRetryPolicy;
@@ -69,7 +72,7 @@ public class LoAdapter {
                         .withOffset(offset * pageSize);
                 List<Group> loGroups = Failsafe.with(groupRetryPolicy)
                         .get(() -> groups.getGroups(groupsFilter));
-                loGroups.forEach(g -> groupsMap.put(g.getPathNode(), g.getId()));
+                loGroups.forEach(g -> groupsMap.put(g.getPathNode(), g));
                 if (loGroups.size() < pageSize) {
                     break;
                 }
@@ -83,23 +86,56 @@ public class LoAdapter {
     public List<Device> getDevices(String groupName) {
         List<Device> devices = new ArrayList<>(pageSize);
         Inventory inventory = loApiClient.getDeviceManagement().getInventory();
-        String groupsMapOrDefault = groupsMap.getOrDefault(groupName, DEFAULT_GROUP_ID);
 
-        for (int offset = 0; ; offset++) {
-            GetDevicesFilter devicesFilter = new GetDevicesFilter()
-                    .withGroupId(groupsMapOrDefault)
-                    .withLimit(pageSize)
-                    .withOffset(offset * pageSize);
-            List<Device> loDevices = Failsafe.with(deviceRetryPolicy)
-                    .get(() -> inventory.getDevices(devicesFilter));
-            LOG.trace("Got {} devices", loDevices.size());
-            devices.addAll(loDevices);
-            if (loDevices.size() < pageSize) {
-                break;
+        List<String> deviceGroups = getGroupWithSubgroups(groupName);
+
+        deviceGroups.forEach(groupId -> {
+            for (int offset = 0; ; offset++) {
+                GetDevicesFilter devicesFilter = new GetDevicesFilter()
+                        .withGroupId(groupId)
+                        .withLimit(pageSize)
+                        .withOffset(offset * pageSize);
+                List<Device> loDevices = Failsafe.with(deviceRetryPolicy)
+                        .get(() -> inventory.getDevices(devicesFilter));
+                LOG.trace("Got {} devices", loDevices.size());
+                devices.addAll(loDevices);
+                if (loDevices.size() < pageSize) {
+                    break;
+                }
             }
-        }
+        });
         LOG.trace("Devices: {}", devices);
         return devices;
+    }
+
+    private List<String> getGroupWithSubgroups(String groupName) {
+        String mainGroupId = getMainDeviceGroupId(groupName);
+        List<String> s = getSubgroupsIds(mainGroupId, groupsMap);
+
+        return Stream.concat(Stream.of(mainGroupId), s.stream()).collect(Collectors.toList());
+    }
+
+    private String getMainDeviceGroupId(String groupName) {
+        Optional<Group> group = Optional.ofNullable(groupsMap.get(groupName));
+        if(!group.isPresent()) {
+            LOG.info("Group {} not found, all devices will be synchronized.", groupName);
+        }
+        return group.map(Group::getId).orElse(DEFAULT_GROUP_ID);
+    }
+
+    public List<String> getSubgroupsIds(String parentId, Map<String, Group> groupsMap) {
+        List<String> groupsIds = new ArrayList<>();
+        List<Group> values = new ArrayList<>(groupsMap.values());
+        for (Group currentGroup : values) {
+            String parentIdOfCurrentGroup = currentGroup.getParentId();
+            if (parentIdOfCurrentGroup != null && parentIdOfCurrentGroup.equalsIgnoreCase(parentId)) {
+                String currentGroupId = currentGroup.getId();
+                groupsIds.add(currentGroupId);
+                List<String> subgroupsOfGivenGroup = getSubgroupsIds(currentGroupId, groupsMap);
+                groupsIds.addAll(subgroupsOfGivenGroup);
+            }
+        }
+        return groupsIds;
     }
 
     public void startListeningForMessages() {
