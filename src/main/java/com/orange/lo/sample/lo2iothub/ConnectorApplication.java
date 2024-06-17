6f0819ed1f421dc3785e794d3aa93246ae771267
Szymon <48949812+szlisiecki@users.noreply.github.com>
2024-06-17 06:39:53 +0200

    Aws metrics (#35)
    * add cloudwatch metrics
    
    * add cloudwatch metrics - add service account
    
    ---------
    
    Co-authored-by: Szymon Lisiecki <szymon.lisiecki@likims.com>

--- src/main/java/com/orange/lo/sample/lo2iothub/ConnectorApplication.java
+++ src/main/java/com/orange/lo/sample/lo2iothub/ConnectorApplication.java
@@ -19,6 +19,8 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
 import software.amazon.awssdk.services.cloudwatch.CloudWatchAsyncClient;
 
 import java.lang.invoke.MethodHandles;
@@ -26,6 +28,7 @@ import java.lang.invoke.MethodHandles;
 @SpringBootApplication
 public class ConnectorApplication {
 
+    private static final String SERVICE_PROFILE_NAME = "service-profile";
     private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
     private final MetricsProperties metricsProperties;
 
@@ -40,7 +43,12 @@ public class ConnectorApplication {
     @Bean
     @Qualifier("counters")
     public MeterRegistry meterRegistry() {
-        CloudWatchMeterRegistry cloudWatchMeterRegistry = new CloudWatchMeterRegistry(cloudWatchConfig(), Clock.SYSTEM, CloudWatchAsyncClient.create());
+        CloudWatchAsyncClient cloudWatchAsyncClient = CloudWatchAsyncClient.builder()
+                .credentialsProvider(ProfileCredentialsProvider.create(SERVICE_PROFILE_NAME))
+                .build();
+
+        CloudWatchMeterRegistry cloudWatchMeterRegistry = new CloudWatchMeterRegistry(cloudWatchConfig(), Clock.SYSTEM, cloudWatchAsyncClient);
+
         cloudWatchMeterRegistry.config()
                 .meterFilter(MeterFilter.deny(id -> !id.getName().startsWith("message")))
                 .commonTags(metricsProperties.getDimensionName(), metricsProperties.getDimensionValue());
